6bddcaf294c295c2d66e73c854787b0b3ff6968d
johnson ooi <ojh5065@gmail.com>
2014-03-09 14:24:16 -0400

    - initial commit

---
+++ src/faceRecognition/Smoother.java
@@ -0,0 +1,16 @@
+package faceRecognition;
+
+import static com.googlecode.javacv.cpp.opencv_core.*;
+import static com.googlecode.javacv.cpp.opencv_imgproc.*;
+import static com.googlecode.javacv.cpp.opencv_highgui.*;
+
+public class Smoother {
+    public static void smooth(String filename) { 
+        IplImage image = cvLoadImage(filename);
+        if (image != null) {
+            cvSmooth(image, image, CV_GAUSSIAN, 3);
+            cvSaveImage(filename, image);
+            cvReleaseImage(image);
+        }
+    }
+}
